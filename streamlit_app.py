b2b76e748d7062f8643fde92a8929ab4a466aacb
Harry He <harry.he.zz@gmail.com>
2023-04-01 16:35:43 -0700

    testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
 st.write("testing to see if this works")
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
